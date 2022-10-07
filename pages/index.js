cf38fd5e5544e4b223a24fe6ef4628d106365aa9
Aradhya-Shaswat <reddye1120@gmail.com>
2022-10-07 10:38:47 +0530

    EOL

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,10 @@ import logo from '../pages/logo.png'
 export default function Home() {
   return (
     <div className="App">
+      <Head>
+        <title>End of Life.</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <header className="App-header">
       <Image src={logo} className="App-logo" alt="logo" width="400" height="400"/>
         <p>
