74ca5446483ac058aa18abd2858389f392e527dd
Aradhya-Shaswat <reddye1120@gmail.com>
2022-10-23 21:09:22 +0530

    WEINER

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import logo from '../pages/logo.png'
 
 export default function Home() {
@@ -27,5 +26,3 @@ export default function Home() {
     </div>
   )
 }
-
-// 
