a77e99f9d736b0a2ddad79ac36eb7a64a54c0ec9
Aradhya-Shaswat <reddye1120@gmail.com>
2022-10-25 17:08:35 +0530

    changed link for unbalanced

--- pages/unbalanced.js
+++ pages/unbalanced.js
@@ -16,7 +16,7 @@ function unBalanced() {
         </p>
         <a
           className="App-link"
-          href="https://doc-0s-4c-docs.googleusercontent.com/docs/securesc/3i6m00t1o7ajcnk3ckqg2ni4f1atj20r/tkvsnmv2378a8hbrt9md2ajrv6fq9v5h/1666539450000/08074060676644573524/08074060676644573524/1zS5Qdy77A10glAs5nkezjaxMr3KJ0vFu?e=download&ax=ALW9-sAZdGXmoKDbt5NVffpy4MScb1KPsGqqUYFpf1wGXNzYXK-0QNkQilW5Ic83H8OmL-9iXUGNZDSU4qhU86DZZ_jbRRZkE4F8ZwZX0vR1Fnj2G7igJAnNk3sHe1O9DtyYsxY9TCcCGY_pEvpidjDIdD3Fd1Aunl2rmQvOF62rkfGqAaDmFicqVv1lr5Ud9Y5xFkg259N19BVFW9rzAF45SYDaOQYxVxXj3H1HPlydfSiU3FcatLGHo6wg3sYWKOJXHzqB600xbcNk8Zplaw3Ly7veC14NKacaZoPSL0Y7yynsdnyhiRxbY9Dl_S1T2kF_y_--MDi9HHnWi1a1zOhbxS4knS-LIKfOflIlVkg9gT_s88-ZnyR_KLnK1EUXXqQJZeN-N-sK3HLOe0LbHr_3PGsyieFIivY8DpayQybNoV_n7Zm67rZ5dUlmqs5_lZD4jfMz_c3MiY5RL45g8vFk92VVV8h20hAV3O0ydF6ci6S2mj8bd1qYzNMqH6fD7usHaRmjMz3hx_v36rvD4J30aMM60FpQ3NcHDMnL4unaQWAxEMnhiCiiNuAbQvPx7dmldbCfMo-UaqI0-bbJZC0ms4lfff1DTP1KSPE1EaVOXB6EitZnvb3ws8p_ykgT_uWoOBIAPj14WIvUllw6tLVk5pqrUmZVoihFetuFqTq1-QNsqKn7Z8NzcMPGmTGff1FcO9xN4krTI6BhwA7uwBOBHSecIjYpnBVSHWLXwsJqvAGq4j_0eyo-zLkrj34l4uR8lEHgeqRygHSkSgLDhwrhgMgRNPKEkfVhnbym59UtmO7PCK5dc56fafgxAMTTiijTP_Psv6f4HuneaXDgQRTsC3deMg9CCiOk79uV9bOGeoJo7M3bKHosO4m9P35Rv00Wh8ThW4eZh5xbIOOAgor1739nBzvLKpmxfBkwchTDFJn4h_7MRmyg_3ybxRkkQb0GHMcPOJvpxd0VOZ9hVRfg8pd5Avndc4cYi3jvjDzwUuDafGQq8Log8LAFEGTyS08LnS3N3J9JMBXBYbBOROOy0pBKXzwyLUgFwA9DR-sH5J4i_1wuoBNC-RUTmA8X9rSX0LSlBCEHMU_jrjlZybh1dEz9uZQJJCyAJEt5FEXoYSLe1QoY1yCPWNB9IrXV&uuid=ec10400d-768c-4250-9b19-590f54ca6343&authuser=0&nonce=qc6v275441p3s&user=08074060676644573524&hash=ohhqmtqu2ng3ldmf5m2eguj6c7jc22ss"
+          href="https://drive.google.com/uc?export=download&id=1RuCdcp1G0CDkj7lbVsXhYXHTrlUQz1fb"
           target=""
           rel=""
         >
